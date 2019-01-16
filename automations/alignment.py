8cae0335b0efdcdeec023e3f0d7bde6d477369d5
michael <michael.kennedy@thedropbears.org.au>
2019-01-16 19:45:23 +1100

    Refactor of alignment to use a hyperbolic curve

--- automations/alignment.py
+++ automations/alignment.py
@@ -2,13 +2,16 @@ import math
 import wpilib
 import numpy as np
 
-from vision import Vision
+from components.vision import Vision
 from magicbot.state_machine import StateMachine, state
-from pyswervedrive.chassis import Chassis
-from utilities.imu import IMU
+from pyswervedrive.swervechassis import SwerveChassis
+from utilities.navx import NavX
+from magicbot import tunable
+
+vision = Vision()
 
 
-class AlignmentStateMachine(StateMachine):
+class Aligner(StateMachine):
     """
     A state machine for alignment using vision systems.
 
@@ -17,120 +20,40 @@ class AlignmentStateMachine(StateMachine):
     tape once we are able to see it.
     """
 
-    vision: Vision
-    chassis: Chassis
-    imu: IMU
+    chassis: SwerveChassis
+    imu: NavX
 
-    def __init__(
-        self,
-        angle_tolerance,
-        closing_step_size,
-        max_target_tape_loops,
-        approach_offset_angle,
-    ):
-        self.angle_tolerance = angle_tolerance
-        self.closing_step_size = closing_step_size
-        self.target_tape_loops = 0
-        self.max_target_tape_loops = max_target_tape_loops
-        self.approach_offset_angle = approach_offset_angle
-        self.close_speed_denominator = 4
-        self.close_side_speed_denominator = 10
+    def setup(self):
+        self.loop_counter = 0
+
+    kP_x = tunable(0.5) # forwards
+    kP_y = tunable(1) # m/s
+    tolerance = tunable(0.05) # % of camera view
 
     @state(first=True)
-    def check_ground(self):
-        """Check for vision tape on the ground"""
-        if self.vision.ground_tape_angle == []:
-            self.next_state_now("ground_align")
-        else:
-            self.next_state_now("target_tape_align")
-
-    @state
-    def ground_tape_align(self):
-        """
-        Align with the objective by way of the ground vision tape.
-
-        The robot will attempt to correct errors untill they are within
-        tolerance, commands a constant forwards velocity with lateral
-        corrections based off the difference between the vision angle and
-        angle measured by the IMU.
-        NOTE: the set_inputs command could exceed the limitations of the
-        hardware, this is handled by the drivebase control system
-        """
-        heading = self.imu.getAngle()
-        if abs(self.vision.ground_tape_angle) > self.angle_tolerance:
-            vision_vector = np.array(
-                math.cos(self.vision.ground_tape_angle),
-                math.sin(self.vision.ground_tape_angle),
-            )
-            if self.vision.ground_tape_angle - heading > 0:
-                rotated_vision_vector = self.rotate(vision_vector, 90)
-            elif self.vision.ground_tape_angle - heading < 0:
-                rotated_vision_vector = self.rotate(vision_vector, -90)
-            else:
-                rotated_vision_vector = vision_vector
-            self.chassis.set_inputs(
-                vision_vector[0] / self.close_speed_denominator
-                + rotated_vision_vector[0] / self.close_side_speed_denominator,
-                vision_vector[1] / self.close_speed_denominator
-                + rotated_vision_vector[1] / self.close_side_speed_denominator,
-                heading + self.vision.ground_tape_angle,
-                # TODO tune these magic numbers
-            )
-        else:
-            self.done()
-
-    @state
-    def target_tape_align(self):
+    def target_tape_align(self, initial_call):
         """
         Align with the objective using the vision tape above the objective.
 
         The robot will try to correct errors untill they are within tolerance
-        by moving and rotating towards the measured vision angle, once
-        successful move on to the ground alignment system.
-        NOTE: the set_inputs command could exceed the limitations of the
-        hardware, this is handled by the drivebase control system
+        by strafing and moving in a hyberbolic curve towards the target.
         """
-        heading = self.imu.getAngle()
-        if abs(self.vision.target_tape_angle) > self.angle_tolerance:
-            vision_vector = np.array(
-                math.cos(self.vision.target_tape_angle),
-                math.sin(self.vision.target_tape_angle),
-            )
-            rotated_vision_vector = self.rotate(
-                vision_vector, self.approach_offset_angle
-            )
-            self.chassis.set_inputs(
-                rotated_vision_vector[0],
-                rotated_vision_vector[1],
-                heading + self.vision.target_tape_angle,
-            )
-        elif self.target_tape_loops >= self.max_target_tape_loops:
-            self.next_state_now("move_closer")
-        else:
-            self.target_tape_loops += 1
-            self.next_state_now("check_ground")
-
-    @state
-    def move_closer(self, initial_call):
-        """
-        Drive towards the target to try to find the ground tape.
-
-        After the robot has failed to spot the ground tape, move forwards
-        transition back to the check ground state
-        """
+        kP_x = self.kP_x
+        kP_y = self.kP_y
+        tolerance = self.tolerance
         if initial_call:
-            starting_odometry = self.chassis.odometry
-        heading = self.imu.getAngle()
-        self.chassis.set_inputs(
-            math.cos(self.vision.target_tape_angle),
-            math.sin(self.vision.target_tape_angle),
-            heading + self.vision.ground_tape_angle,
-        )
-        if self.chassis.odometry - starting_odometry > self.closing_step_size:
-            self.next_state_now("check_ground")
-
-    def rotate(self, A, angle):
-        rotation_matrix = np.array(
-            [[math.cos(angle), math.sin(angle)], [-math.sin(angle), math.cos(angle)]]
-        )
-        return A @ rotation_matrix
+            self.loop_counter = 0
+        error = vision.get_target_tape_error()
+        if error is None:
+            self.loop_counter += 1
+            if self.loop_counter > 3:
+                self.chassis.set_inputs(0, 0, 0)
+                self.done()
+                return
+        else:
+            if abs(error) > tolerance:
+                self.chassis.set_inputs(
+                    (1-abs(error))*kP_x, error*kP_y, 0, field_oriented=False
+                )
+            else:
+                self.done()
