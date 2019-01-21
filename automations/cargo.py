623cc566168e3dec5f2db7e0d76ae4911e02b620
chocolatejade42 <Aiden.Gardner@icloud.com>
2019-01-21 15:16:51 +1100

    Beginning of cargo state machine

--- automations/cargo.py
+++ automations/cargo.py
@@ -10,48 +10,50 @@ class CargoManager(AlignBase):
     arm: Arm
     intake: Intake
 
-    def move_to_cargo_ship(self, force=False):
-        self.engage(initial_state="moving_cargo_ship", force=force)
+    def start_intake(self, force=False):
+        self.engage(initial_state="lower_floor", force=force)
 
     @state(first=True)
-    def moving_cargo_ship(self):
-        self.arm.raise_bot_ext()
-        self.arm.raise_top_ext()
-        self.done()
-
-    def move_to_rocket_ship(self, force=False):
-        self.engage(initial_state="moving_rocket_ship", force=force)
-
-    @state
-    def moving_rocket_ship(self):
-        self.arm.raise_bot_ext()
-        self.arm.lower_top_ext()
-        self.done()
-
-    def move_to_floor(self, force=False):
-        self.engage(initial_state="moving_floor", force=force)
-
-    @state
-    def moving_floor(self):
+    def lower_floor(self):
         self.arm.lower_bot_ext()
         self.arm.lower_top_ext()
-
-    def start_intake(self, force):
-        self.engage(initial_state="aligning", force=force)
-
-    @state
-    def successful_align(self):
-        print("successful align")
         self.next_state("intaking_cargo")
-
-    @state
-    def failed_align(self):
-        print("failed align")
-        self.done()
 
     @state(must_finish=True)
     def intaking_cargo(self):
         self.intake.intake()
         if self.intake.contained():
-            self.intake.stop_intake()
+            self.intake.stop()
+            self.done()
+
+    def outtake_cargo_rocket(self, force=False):
+        self.engage(initial_state="raise_rocket", force=force)
+
+    @state
+    def raise_rocket(self):
+        self.arm.lower_top_ext()
+        self.arm.raise_bot_ext()
+        self.next_state_now("aligning")
+
+    def outtake_cargo_ship(self, force=False):
+        self.engage(initial_state="raise_cargo", force=force)
+
+    @state
+    def raise_cargo(self):
+        self.arm.raise_top_ext()
+        self.arm.raise_bot_ext()
+        self.next_state_now("aligning")
+
+    @state
+    def successful_align(self):
+        self.next_state_now("outtaking_cargo")
+
+    @state
+    def failed_align(self):
+        self.done()
+
+    @state(must_finish=True)
+    def outtaking_cargo(self):
+        self.intake.outtake()
+        if not self.intake.contained():
             self.done()
--- components/intake.py
+++ components/intake.py
@@ -1,36 +1,35 @@
 import ctre
 import wpilib
 
+
 class Intake:
-  
-  intake_motor: ctre.TalonSRX
 
-  def __init__(self):
-    self.last_motor_output = None
-    self.motor_output = None
-  
-  def setup(self):
-    pass
-  
-  def execute(self):
-    if self.motor_output != self.last_motor_output:
-        self.intake_motor.set(ctre.ControlMode.PercentOutput, self.motor_output)
-    self.last_motor_output = self.motor_output
+    intake_motor: ctre.TalonSRX
+    intake_switch: wpilib.DigitalInput
 
-  def intake(self):
-    self.motor_output = -1
+    def __init__(self):
+        self.last_motor_output = None
+        self.motor_output = 0
 
-  def outtake(self):
-    self.motor_output = 1
-  
-  def toggle(self):
-    if self.motor_output:
-      self.motor_output *= -1
-    else:
-      self.motor_output = -1
-  
-  def stop(self):
-    self.motor_output = 0
-  
-  def emergency_stop(self):
-    self.intake_motor.stopMotor()
+    def setup(self):
+        pass
+
+    def execute(self):
+        if self.motor_output != self.last_motor_output:
+            self.intake_motor.set(ctre.ControlMode.PercentOutput, self.motor_output)
+        self.last_motor_output = self.motor_output
+
+    def intake(self):
+        self.motor_output = -1
+
+    def outtake(self):
+        self.motor_output = 1
+
+    def stop(self):
+        self.motor_output = 0
+
+    def emergency_stop(self):
+        self.intake_motor.stopMotor()
+
+    def contained(self):
+        return True  # self.intake_switch.get()
