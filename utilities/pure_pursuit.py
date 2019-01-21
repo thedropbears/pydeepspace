34cc5a897cc92793a0a01b6f8969cfb69e302459
michael <michael.kennedy@thedropbears.org.au>
2019-01-21 16:29:09 +1100

    Implemented transition between segments.

--- utilities/pure_pursuit.py
+++ utilities/pure_pursuit.py
@@ -12,21 +12,22 @@ class PurePursuit:
     """
     chassis: SwerveChassis
 
-    def __init__(self, look_ahead, speed_modifier):
-        self.segments = None
+    def __init__(self, look_ahead, speed_modifier, ending_tolerance):
+        self.waypoints = []
         self.look_ahead = look_ahead
-        self.current_segment = None
         self.speed_modifier = speed_modifier
+        self.ending_tolerance = ending_tolerance
+        self.current_waypoint_number = 0
 
-    def find_intersections(self, segment):
+    def find_intersections(self, waypoint_start, waypoint_end):
         """
         Find the intersection/s between our lookahead distance and path.
 
         http://mathworld.wolfram.com/Circle-LineIntersection.html
         NOTE: this will return the intersections in global co-ordinates
         """
-        x_1, y_1 = segment[0], segment[1]
-        x_2, y_2 = segment[2], segment[3]
+        x_1, y_1 = waypoint_start[0], waypoint_start[1]
+        x_2, y_2 = waypoint_end[2], waypoint_end[3]
         goal_point = np.array((x_2, y_2))
 
         d_x = x_2 - x_1
@@ -59,15 +60,27 @@ class PurePursuit:
                 return intersection_2
 
     def build_path(self, waypoints):
-        # check if we actually need to create seperate segments or if we can simply use the waypoints
-        self.segments = waypoints
+        self.waypoints = waypoints
 
     def follow_path(self):
-        goal_point = self.find_intersections(self.current_segment)
+        """Find the goal_point and convert it to relative co-ordinates"""
+        goal_point = self.find_intersections(self.waypoints[self.current_waypoint_number], self.waypoints[self.current_waypoint_number])
         goal_point -= self.chassis.position()
         goal_point = self.chassis.robot_orient(goal_point)
+        goal_point = goal_point / np.linalg.norm(goal_point)
+        self.check_progress()
         self.chassis.set_inputs(*goal_point*self.speed_modifier, 0)
         # need to find a better way to scale the input to the chassis
+
+    def check_progress(self, segment):
+        """Check if we are close enough to begin the path to the next waypoint"""
+        end_point_x, end_point_y = segment[2], segment[3]
+        robot_x, robot_y = *self.chassis.position()
+        difference_x = abs(end_point_x) - abs(robot_x)
+        difference_y = abs(end_point_y) - abs(robot_y)
+        if math.sqrt(difference_x ** 2 + difference_y ** 2) < self.ending_tolerance:
+            self.current_waypoint_number += 1
+
 
     def sgn(self, number):
         """Returns the sign of a number, 0 is positive"""
