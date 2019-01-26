3e9339da7b74d4193c1ac71d92b60fd4ebbac0da
Sam Kerrison <sam.kerrison@thedropbears.org.au>
2019-01-26 16:37:17 +1100

    Add ground tape alignment

--- automations/alignment.py
+++ automations/alignment.py
@@ -19,9 +19,22 @@ class Aligner(StateMachine):
     def setup(self):
         self.loop_counter = 0
 
-    kP_x = tunable(0.5)  # forwards
-    kP_y = tunable(1)  # m/s
-    tolerance = tunable(0.05)  # % of camera view
+    target_tape_kP_x = tunable(0.5)  # forwards
+    target_tape_kP_y = tunable(1)  # m/s
+    target_tape_tolerance = tunable(0.05)  # % of camera view
+
+    ground_tape_kP_x = tunable(0.5)  # forward
+    ground_tape_kP_y = tunable(1)  # m/s
+    ground_tape_kP_angle = tunable(2) 
+    ground_tape_distance_tolerance_y = tunable(0.02)
+    # ground_tape_distance_tolerance_x = tunable(0.4) 
+    ground_tape_angle_tolerance = tunable(math.pi / 360)  # this equals 0.5 degres
+
+    def align(self, deposit, intake):
+        self.align_for_deposit = deposit
+        self.align_for_intake = intake
+
+
 
     @state(first=True)
     def target_tape_align(self, initial_call):
@@ -44,9 +57,88 @@ class Aligner(StateMachine):
                 self.done()
                 return
         else:
-            if abs(error) > tolerance:
+            if (
+                self.vision.get_ground_tape_error_x
+                or self.vision.get_ground_tape_error_y is not None
+            ):
+                if self.align_for_deposit == True:
+                    self.next_state_now("ground_tape_align_deposit")
+                elif self.align_for_intake == True:
+                    self.next_state_now("ground_tape_align_intake")
+                else:
+                    self.chassis.set_inputs(0, 0, 0)
+                    self.done()
+                    return
+
+            elif abs(error) > target_tape_tolerance:
                 self.chassis.set_inputs(
-                    (1 - abs(error)) * kP_x, error * kP_y, 0, field_oriented=False
+                    (1 - abs(error)) * target_tape_kP_x,
+                    error * target_tape_kP_y,
+                    0,
+                    field_oriented=False,
                 )
-            else:
+
+    @state
+    def ground_tape_align_deposit(self, initial_call):
+
+        ground_tape_angle_tolerance = self.ground_tape_angle_tolerance
+        ground_tape_distance_tolerance_y = self.ground_tape_distance_tolerance_y
+
+        if initial_call:
+            self.loop_counter = 0
+        error_x = self.vision.get_ground_tape_error_x
+        error_y = self.vision.get_ground_tape_error_y
+        error_angle = self.vision.get_ground_tape_error_angle
+        if error_x or error_y is None:
+            self.loop_counter += 1
+            if self.loop_counter > 3:
+                self.chassis.set_inputs(0, 0, 0)
                 self.done()
+                return
+        else:
+            if self.vision.get_target_tape_deposting_ragne == True:
+                error_x = 0
+            elif abs(error_y) <= ground_tape_distance_tolerance_y:
+                error_y = 0
+            elif abs(error_angle) <= ground_tape_angle_tolerance:
+                error_angle = 0
+            elif error_x and error_y and error_angle == 0:
+                return True
+                self.done()
+            else:
+                self.chassis.set_inputs(
+                    error_x * self.ground_tape_kP_x,
+                    error_y * self.ground_tape_kP_y,
+                    error_angle * self.ground_tape_kP_angle,
+                    field_oriented=False,
+                )
+    @state
+    def ground_tape_align_intake(self, initial_call):
+        if initial_call:
+            self.loop_counter = 0
+        error_x = self.vision.get_ground_tape_error_x
+        error_y = self.vision.get_ground_tape_error_y
+        error_angle = self.vision.get_ground_tape_error_angle
+        if error_y is None:
+            self.loop_counter += 1
+            if self.loop_counter > 3:
+                self.chassis.set_inputs(0, 0, 0)
+                self.done()
+                return
+        else:
+            if abs(error_x) < self.vision.get_target_tape_deposting_ragne:
+                error_x = 0
+            elif abs(error_y) <= self.ground_tape_distance_tolerance_y:
+                error_y = 0
+            elif abs(error_angle) <= self.ground_tape_angle_tolerance:
+                error_angle = 0
+            elif error_x and error_y and error_angle == 0:
+                return True
+                self.done()
+            else:
+                self.chassis.set_inputs(
+                    error_x * self.ground_tape_kP_x,
+                    error_y * self.ground_tape_kP_y,
+                    error_angle * self.ground_tape_kP_angle,
+                    field_oriented=False,
+                )
--- components/vision.py
+++ components/vision.py
@@ -12,12 +12,52 @@ class Vision:
             NetworkTables.NotifyFlags.NEW | NetworkTables.NotifyFlags.UPDATE,
         )
 
+        self.target_tape_deposting_range = self.nt.getEntry("target_tape_deposting_range")
+        self.target_tape_deposting_range.addListener(
+            self.new_target_value_deposting_range,
+            NetworkTables.NotifyFlags.NEW | NetworkTables.NotifyFlags.UPDATE,
+        )
+
+        self.ground_tape_error_x = self.nt.getEntry("ground_tape_error_x")
+        self.ground_tape_error_x.addListener(
+            self.new_ground_value_y,
+            NetworkTables.NotifyFlags.NEW | NetworkTables.NotifyFlags.UPDATE,
+        )
+
+        self.ground_tape_error_y = self.nt.getEntry("ground_tape_error_y")
+        self.ground_tape_error_y.addListener(
+            self.new_ground_value_y,
+            NetworkTables.NotifyFlags.NEW | NetworkTables.NotifyFlags.UPDATE,
+        )
+
+        self.ground_tape_error_angle = self.nt.getEntry("ground_tape_error_angle")
+        self.ground_tape_error_angle.addListener(
+            self.new_ground_value_angle,
+            NetworkTables.NotifyFlags.NEW | NetworkTables.NotifyFlags.UPDATE,
+        )
+
         # network tables doesn't like none, so we use out of bound values
         self.target_tape_error_value = 999
 
     def new_target_value(self, entry, key, value, param):
         # self.time = time.monotonic()
         self.target_tape_error_value = value
+
+    def new_target_value_deposting_range(self, entry, key, value, param):
+        # self.time = time.monotonic()
+        self.target_tape_deposting_range_value = value
+
+    def new_groound_value_x(self, entry, value, parma):
+        # self.time = time.monotonic()
+        self.ground_tape_error_value_x = value
+
+    def new_ground_value_y(self, entry, key, value, param):
+        # self.time = time.monotonic()
+        self.ground_tape_error_value_y = value
+
+    def new_ground_value_angle(self, entry, key, value, param):
+        # self.time = time.monotonic()
+        self.ground_tape_error_value_angle = value
 
     def get_target_tape_error(self):
         if not -1 <= self.target_tape_error_value <= 1:
@@ -26,3 +66,33 @@ class Vision:
             return 0
         else:
             return self.target_tape_error_value
+
+    def get_target_tape_deposting_ragne(self):
+        if self.target_tape_deposting_range == True:
+            return True
+        else:
+            return False
+
+    def get_ground_tape_error_x(self):
+        if not -1 <= self.ground_tape_error_value_x <= 1:
+            return None
+        elif hal.isSimulation():
+            return 0
+        else:
+            return self.ground_tape_error_value_x
+
+    def get_ground_tape_error_y(self):
+        if not -1 <= self.ground_tape_error_value_y <= 1:
+            return None
+        elif hal.isSimulation():
+            return 0
+        else:
+            return self.ground_tape_error_value_y
+
+    def get_ground_tape_error_angle(self):
+        if not -1 <= self.ground_tape_error_value_angle <= 1:
+            return None
+        elif hal.isSimulation():
+            return 0
+        else:
+            return self.ground_tape_error_value_angle
