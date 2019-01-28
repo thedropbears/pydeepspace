e3b2de658e2e892dfa54d36d73670d76c2e8c6c0
michael <michael.kennedy@thedropbears.org.au>
2019-01-29 10:20:53 +1100

    Added ability to handle loss of vision, removed ground tape code
    During testing, the robot was able to align reliably without using the
    tape

--- automations/alignment.py
+++ automations/alignment.py
@@ -1,7 +1,6 @@
 import math
 
 from components.vision import Vision
-from components.hatch import Hatch
 from magicbot.state_machine import StateMachine, state
 from pyswervedrive.swervechassis import SwerveChassis
 from magicbot import tunable
@@ -15,10 +14,10 @@ class Aligner(StateMachine):
     objectives from longer range and fine adjustment using the ground
     tape once we are able to see it.
     """
+    VERBOSE_LOGGING = True
 
     chassis: SwerveChassis
     vision: Vision
-    hatch: Hatch
 
     def setup(self):
         self.loop_counter = 0
@@ -32,69 +31,38 @@ class Aligner(StateMachine):
     target_tape_kP_y = tunable(1)  # m/s
     target_tape_tolerance = tunable(0.05)  # % of camera view
 
-    ground_tape_kP_y = tunable(1)  # m/s
-    ground_tape_kP_angle = tunable(2)
-    ground_tape_distance_tolerance_y = tunable(0.02)
-    ground_tape_angle_tolerance = tunable(math.pi / 360)  # this equals 0.5 degres
-
     @state(first=True)
-    def target_tape_align(self, initial_call):
+    def target_tape_align(self, initial_call, state_tm):
         """
         Align with the objective using the vision tape above the objective.
 
         The robot will try to correct errors untill they are within tolerance
         by strafing and moving in a hyberbolic curve towards the target.
         """
+        self.chassis.heading_hold_off()
         if initial_call:
             self.loop_counter = 0
+            self.successful = False
         error = self.vision.get_target_tape_error()
         if error is None:
-            self.loop_counter += 1
-            if self.loop_counter > 3:
+            self.chassis.set_inputs(0, -0.5, 0, field_oriented=False)
+            if state_tm > 2.5:
+                self.successful = True
                 self.chassis.set_inputs(0, 0, 0)
-                self.successful = False
                 self.done()
-                return
-        if self.vision.get_ground_tape_y is not None:
-            self.next_state_now("ground_tape_align")
-        if abs(error) > self.target_tape_tolerance:
-            vy = error * self.target_tape_kP_y
         else:
-            vy = 0
-        vx = (1 - abs(error)) * self.target_tape_kP_x
-        self.chassis.set_inputs(vx, vy, 0, field_oriented=False)
+            # if self.vision.get_ground_tape_y is not None:
+            #     self.next_state_now("ground_tape_align")
+            vy = error * self.target_tape_kP_y
+            vx = (1 - abs(error)) * self.target_tape_kP_x
+            self.chassis.set_inputs(vy, -vx, 0, field_oriented=False)
+            if self.vision.within_deposit_range:
+                self.next_state("success")
 
     @state
-    def ground_tape_align(self, initial_call):
-        if initial_call:
-            self.loop_counter = 0
-        error_y = self.vision.get_ground_tape_y
-        error_angle = self.vision.get_ground_tape_angle()
-        if error_y is None:
-            self.loop_counter += 1
-            if self.loop_counter > 3:
-                self.chassis.set_inputs(0, 0, 0)
-                self.successful = False
-                self.done()
-        else:
-            if abs(error_angle) <= self.ground_tape_angle_tolerance:
-                error_angle = 0
-            else:
-                self.chassis.set_inputs(0, 0, error_angle * self.ground_tape_kP_angle)
-            if abs(error_y) <= self.ground_tape_distance_tolerance_y:
-                error_y = 0
-            else:
-                self.chassis.set_inputs(0, 0, error_y * self.ground_tape_kP_y)
-            if self.mode == self.hatch_deposit:
-                if self.vision.within_deposit_range:
-                    vx = 0
-                else:
-                    vx = 0.5
-            elif self.mode == self.hatch_intake:
-                if self.hatch.contained():
-                    vx = 0
-                else:
-                    vx = 0.5
-            if vx and error_y and error_angle == 0:
-                self.successful = True
-                self.done()
+    def success(self, state_tm):
+        self.chassis.set_inputs(0, -0.5, 0, field_oriented=False)
+        if state_tm > 0.5:
+            self.successful = True
+            self.chassis.set_inputs(0, 0, 0)
+            self.done()
