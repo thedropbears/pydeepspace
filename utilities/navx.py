755ac5ce3cde0c0119e34105869266290405c9b7
David Vo <auscompgeek@users.noreply.github.com>
2019-01-31 00:49:23 +1100

    Clean up navX wrapper

--- utilities/navx.py
+++ utilities/navx.py
@@ -1,42 +1,28 @@
 import math
 
-from navx import AHRS
-from wpilib.interfaces import PIDSource
+import navx
 
 
 class NavX:
-    """Wrapper around RobotPy NavX to be compatible with our BNO055 driver."""
-
-    PIDSourceType = PIDSource.PIDSourceType
+    """Wrapper around RobotPy NavX to match our coordinate system."""
 
     def __init__(self):
-        self.ahrs = AHRS.create_spi(update_rate_hz=50)
+        self.ahrs = navx.AHRS.create_spi(update_rate_hz=50)
         self.pidsource = self.PIDSourceType.kDisplacement
 
     def getAngle(self) -> float:
-        """Get NavX angle.
+        """Get the current yaw in radians.
 
-        Returns:
-            Angle in radians between -pi and +pi.
+        Angles are in the interval [-pi, pi], anticlockwise positive.
         """
         raw = self.ahrs.getYaw()
         return -math.radians(raw)
 
     def resetHeading(self):
+        """Zero the yaw."""
         self.ahrs.reset()
 
     def getHeadingRate(self):
-        # multiply by 100 because NavX does not normalise per timestep
+        """Get the rate of change of yaw in radians per second."""
+        # multiply by update freq because NavX does not normalise per timestep
         return math.radians(-self.ahrs.getRate()) * 50
-
-    def pidGet(self):
-        if self.pidsource == self.PIDSourceType.kDisplacement:
-            return self.getAngle()
-        else:
-            return self.getHeadingRate()
-
-    def setPIDSourceType(self, pidsourcetype):
-        self.pidsource = pidsourcetype
-
-    def getPIDSourceType(self):
-        return self.pidsource
