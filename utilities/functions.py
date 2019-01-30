ceb8f1e7e952885dc556695437a60ed2edfafd88
David Vo <auscompgeek@users.noreply.github.com>
2019-01-31 00:46:11 +1100

    Clean up utility functions

--- utilities/functions.py
+++ utilities/functions.py
@@ -2,37 +2,32 @@ import math
 
 
 def rescale_js(
-    value, deadzone: float = 0.0, exponential: float = 0.0, rate: float = 1.0
-):
-    """Rescale the joystick inputs, applying a deadzone, exponential, and max rate.
+    value: float, deadzone: float = 0, exponential: float = 0, rate: float = 1
+) -> float:
+    """Rescale a joystick input, applying a deadzone, exponential, and max rate.
 
     Args:
-        value: the joystick value, between + and - 1.
+        value: the joystick value, in the interval [-1, 1].
         deadzone: the deadzone to apply.
-        exponential: the strength of the exponential to apply (ie how non
-        linear should the response be)
-        rate: the max rate to return (ie the value to be returned when 1.0 is given)
-    Returns:
-        The joystick value.
+        exponential: the strength of the exponential to apply
+                     (i.e. how non linear should the response be)
+        rate: the max rate to return (i.e. the value to be returned when 1 is given)
     """
-    value_negative = 1.0
+    sign = 1
     if value < 0:
-        value_negative = -1.0
+        sign = -1
         value = -value
-    # Cap to be +/-1
-    if abs(value) > 1.0:
-        value /= abs(value)
     # Apply deadzone
-    if abs(value) < deadzone:
-        return 0.0
-    elif exponential == 0.0:
+    if value < deadzone:
+        return 0
+    if not exponential:
         value = (value - deadzone) / (1 - deadzone)
     else:
         a = math.log(exponential + 1) / (1 - deadzone)
         value = (math.exp(a * (value - deadzone)) - 1) / exponential
-    return rate * value_negative * value
+    return rate * sign * value
 
 
-def constrain_angle(angle):
-    """Wrap :param angle: to between +pi and -pi"""
+def constrain_angle(angle: float) -> float:
+    """Wrap an angle to the interval [-pi, pi]."""
     return math.atan2(math.sin(angle), math.cos(angle))
