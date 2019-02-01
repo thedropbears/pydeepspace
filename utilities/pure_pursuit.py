b869d46c86d93cddd45d571dc2e7491211be4329
michael <michael.kennedy@thedropbears.org.au>
2019-02-01 17:28:42 +1100

    Pursuit: Calculate distance along path

--- utilities/pure_pursuit.py
+++ utilities/pure_pursuit.py
@@ -1,13 +1,13 @@
 import math
 import numpy as np
+
 # from magicbot import tunable
 
 
 class PurePursuit:
     """
     Pure Pursuit controller for navigation with absolute waypoints
-
-    Uses the method outlined here
+    Uses the method outlined here with some changes to be suitible for a swervedrive
     https://www.ri.cmu.edu/pub_files/pub3/coulter_r_craig_1992_1/coulter_r_craig_1992_1.pdf
     """
 
@@ -15,16 +15,16 @@ class PurePursuit:
     # speed_modifier = tunable(0)
     # ending_tolerance = tunable(0) # m
 
-    def __init__(self, look_ahead, ending_tolerance):
+    def __init__(self, look_ahead):
         self.waypoints = []
         self.current_waypoint_number = 0
         self.look_ahead = look_ahead
-        self.ending_tolerance = ending_tolerance
+        self.completed_path = False
+        self.distance_traveled = 0
 
     def find_intersections(self, waypoint_start, waypoint_end, robot_position):
         """
         Find the intersection/s between our lookahead distance and path.
-
         http://mathworld.wolfram.com/Circle-LineIntersection.html
         NOTE: this will return the intersections in global co-ordinates
         """
@@ -35,6 +35,7 @@ class PurePursuit:
         x_1 -= robot_x
         y_2 -= robot_y
         y_1 -= robot_y
+        segment_end = np.array((x_2, y_2))
 
         d_x = x_2 - x_1
         d_y = y_2 - y_1
@@ -62,8 +63,8 @@ class PurePursuit:
                 return intersection_1
             intersection_2[0] = (left_x - right_x) / denominator
             intersection_2[1] = (left_y - right_y) / denominator
-            if np.linalg.norm((intersection_1) - (waypoint_end)) < np.linalg.norm(
-                (intersection_2) - (waypoint_end)
+            if np.linalg.norm((intersection_1) - (segment_end)) < np.linalg.norm(
+                (intersection_2) - (segment_end)
             ):
                 return intersection_1
             else:
@@ -71,54 +72,47 @@ class PurePursuit:
         else:
             print("No intersection found")
 
-    def build_path(self, waypoints: np.ndarray):
-        self.waypoints = waypoints
+    def build_path(self, waypoints):
+        """
+        Take in a list of waypoints used to build a path.
 
-    def compute_direction(self, robot_position):
+        The waypoints must be a tuple (x, y, speed), this method will
+        create waypoints with these co-ordinates and distance
+        along the path from the start of the trajectory.
+        """
+        self.last_robot_x = waypoints[0][0]
+        self.last_robot_y = waypoints[0][1]
+        self.completed_path = False
+        self.distance_traveled = 0
+        self.waypoints = []
+        waypoint_distance = 0
+        previous_waypoint = waypoints[0]
+        for waypoint in waypoints:
+            x, y, speed = waypoint
+            waypoint_distance += math.hypot(
+                x - previous_waypoint[0], y - previous_waypoint[1]
+            )
+            previous_waypoint = waypoint
+            self.waypoints.append((x, y, speed, waypoint_distance))
+        self.current_waypoint_number = 0
+        print(self.waypoints)
+
+    def compute_direction(
+        self, robot_position, segment_start, segment_end, distance_along_path
+    ):
         """Find the goal_point and convert it to relative co-ordinates"""
-        if self.current_waypoint_number + 1 >= len(self.waypoints):
-            return
-        goal_point = self.find_intersections(
-            self.waypoints[self.current_waypoint_number],
-            self.waypoints[self.current_waypoint_number + 1],
-            robot_position
-        )
+        goal_point = self.find_intersections(segment_start, segment_end, robot_position)
         if goal_point is None:
             # if we cant find an intersection between the look_ahead and path
-            # use the closest point on the segment as our goal
-            
-            goal_point = self.find_closest_path_point(self.waypoints[self.current_waypoint_number],
-            self.waypoints[self.current_waypoint_number + 1], robot_position)
-        # goal_point -= robot_position[:2]
-        # goal_point = self.robot_orient(*goal_point, robot_position[2])
-        print(goal_point)
-        self.goal_point = goal_point
-        changed_waypoint = self.check_progress(self.waypoints[self.current_waypoint_number + 1], robot_position)
-        return goal_point, changed_waypoint
-
-
-    def check_progress(self, end_waypoint, robot_position):
-        """Check if we are close enough to begin the path to the next end_waypoint"""
-        end_point_x, end_point_y = end_waypoint[0], end_waypoint[1]
-        robot_x, robot_y, _ = robot_position
-        difference_x = abs(end_point_x) - abs(robot_x)
-        difference_y = abs(end_point_y) - abs(robot_y)
-        if math.sqrt(difference_x ** 2 + difference_y ** 2) < self.ending_tolerance:
+            # use the next waypoint as our goal point
+            goal_point = segment_end[:2]
+        # print(goal_point)
+        if self.distance_traveled >= segment_end[3]:
+            # if we have reached the end of our current segment
             self.current_waypoint_number += 1
-            return True
-
-    def find_closest_path_point(self, segment_start, segment_end, robot_position):
-        """
-        Finds the closest point to the robot on the path.
-        """
-        x_1, y_1 = segment_start
-        x_2, y_2 = segment_end
-        x_robot, y_robot, _ = robot_position
-        m = (y_1 - y_2) / (x_1 - x_2)
-        m_orth = -1 / m
-        x = (y_1 - y_robot + m_orth * x_robot - m * x_1) / (m_orth - m)
-        y = m * (x - x_1) + y_1
-        return x, y
+            print("changed segment")
+        goal_point = goal_point / np.linalg.norm(goal_point)
+        return goal_point
 
     def sgn(self, number):
         """Returns the sign of a number, 0 is positive"""
@@ -127,18 +121,55 @@ class PurePursuit:
         else:
             return 1
 
-    def robot_orient(self, x, y, heading):
-        """Turn a vx and vy relative to the field into a vx and vy based on the
-        robot.
-
-        Args:
-            vx: vx to robot orient
-            vy: vy to robot orient
-            heading: current heading of the robot. In radians CCW from +x axis.
-        Returns:
-            float: robot oriented vx speed
-            float: robot oriented vy speed
+    def distance_along_path(self, robot_position):
         """
-        oriented_x = x * math.cos(heading) + y * math.sin(heading)
-        oriented_y = -x * math.sin(heading) + y * math.cos(heading)
-        return oriented_x, oriented_y
+        Find the robots position on the path using odometry.
+        Every timestep, add the distance the robot has travelled to a
+        running total used to check for waypoints.
+        """
+        robot_x, robot_y, _ = robot_position
+        self.distance_traveled += math.hypot(
+            robot_x - self.last_robot_x, robot_y - self.last_robot_y
+        )
+        self.last_robot_x = robot_x
+        self.last_robot_y = robot_y
+        # print(self.distance_traveled)
+        return self.distance_traveled
+
+    def find_speed(
+        self,
+        start_path_distance,
+        end_path_distance,
+        start_speed,
+        end_speed,
+        distance_along_path,
+    ):
+        """
+        Find the how fast the robot should be moving at it's current point.
+        """
+        local_robot_distance = distance_along_path - start_path_distance
+        local_end_distance = end_path_distance - start_path_distance
+        speed_difference = end_speed - start_speed
+        portion_path_completed = local_robot_distance / local_end_distance
+        target_speed = speed_difference * portion_path_completed + start_speed
+        return target_speed
+
+    def find_velocity(self, robot_position):
+        if self.current_waypoint_number >= len(self.waypoints) - 1:
+            self.completed_path = True
+            print("path completed")
+            return None, None, None
+        distance_along_path = self.distance_along_path(robot_position)
+        segment_start = self.waypoints[self.current_waypoint_number]
+        segment_end = self.waypoints[self.current_waypoint_number + 1]
+        start_speed, start_distance = segment_start[2:]
+        end_speed, end_distance = segment_end[2:]
+        direction = self.compute_direction(
+            robot_position, segment_start, segment_end, distance_along_path
+        )
+        speed = self.find_speed(
+            start_distance, end_distance, start_speed, end_speed, distance_along_path
+        )
+        vx, vy = direction * speed
+        heading = 0
+        return vx, vy, heading
