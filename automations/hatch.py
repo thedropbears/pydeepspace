2f90de4c6fe6db6110cace3eaf77febf2e2d1a80
michael <michael.kennedy@thedropbears.org.au>
2019-02-01 19:15:55 +1100

    Add aligning state to hatch statemachine

--- automations/hatch.py
+++ automations/hatch.py
@@ -1,14 +1,23 @@
-from components.hatch import Hatch
 from magicbot import StateMachine, state, timed_state
+
+from automations.alignment import Aligner
+from components.hatch import Hatch
 
 
 class HatchController(StateMachine):
     hatch: Hatch
+    align: Aligner
 
     def punch(self, force=False):
         self.engage(force=force)
 
     @state(first=True, must_finish=True)
+    def aligning(self, initial_call):
+        self.align.engage()
+        if self.align.successful and not self.align.is_executing:
+            self.next_state("punching")
+
+    @state(must_finish=True)
     def punching(self):
         self.hatch.punch()
         self.next_state("retracting")
