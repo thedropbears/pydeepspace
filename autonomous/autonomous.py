e6b763d6dd800bfaf98a90e88042af894840a33d
michael <michael.kennedy@thedropbears.org.au>
2019-02-03 11:35:34 +1100

    Autonomous: Update coordinates to include heading

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -2,28 +2,26 @@ import math
 
 from magicbot.state_machine import AutonomousStateMachine, state
 
-from automations.hatch import HatchController
+from automations.alignment import HatchDepositAligner, HatchIntakeAligner
 from components.hatch import Hatch
 from components.vision import Vision
 from pyswervedrive.chassis import SwerveChassis
 from utilities.navx import NavX
 from utilities.pure_pursuit import PurePursuit
 
-from automations.alignment import Aligner
-
 
 def reflect_2d_y(v: tuple) -> tuple:
-    return (v[0], -v[1])
+    return (v[0], -v[1], v[2], v[3])
 
 
 class AutoBase(AutonomousStateMachine):
-    DEFAULT = False
 
     # Here magicbot injects components
-    align: Aligner
+    hatch_deposit: HatchDepositAligner
+    hatch_intake: HatchIntakeAligner
+
     chassis: SwerveChassis
     hatch: Hatch
-    hatchman: HatchController
     imu: NavX
     vision: Vision
 
@@ -32,36 +30,37 @@ class AutoBase(AutonomousStateMachine):
 
     def __init__(self):
         super().__init__()
-        self.front_cargo_bay = (5.6 - SwerveChassis.LENGTH / 2 - 0.5, 0.2, 0.2)
-        self.setup_loading_bay = (3.3, 2.2, 2)
-        self.loading_bay = (0.2 + SwerveChassis.LENGTH / 2 + 0.5, 3.38, 0.2)
-        self.loading_bay_speedup_point = (1 + SwerveChassis.LENGTH / 2, 3.38, 2)
-        self.side_cargo_bay = (6.6, 0.9 + SwerveChassis.WIDTH / 2 + 0.5, 0.2)
-        self.side_cargo_bay_slowdown_point = (6.6, 1.6 + SwerveChassis.WIDTH / 2, 1.5)
+        self.front_cargo_bay = (5.6 - SwerveChassis.LENGTH / 2 - 0.5, 0.2, 0, 0.2)
+        self.setup_loading_bay = (3.3, 3.3, math.pi, 2)
+        self.loading_bay = (0.2 + SwerveChassis.LENGTH / 2 + 0.5, 3.38, math.pi, 0.2)
+        self.loading_bay_speedup_point = (1 + SwerveChassis.LENGTH / 2, 3.38, math.pi, 2)
+        self.side_cargo_bay = (6.6, 0.9 + SwerveChassis.WIDTH / 2 + 0.5, -math.pi/2, 0.2)
+        self.side_cargo_bay_slowdown_point = (6.6, 1.6 + SwerveChassis.WIDTH / 2, -math.pi/2, 1.5)
         # The waypoints we use to move to the other side of the field
-        self.cross_point = (4.7, 1, 2)
+        self.cross_point = (4.7, 1, 0, 2)
         # points on opposite side of field
         # self.opp_front_cargo_bay = reflect_2d_y(self.front_cargo_bay)
         # self.opp_setup_loading_bay = reflect_2d_y(self.setup_loading_bay)
         self.opp_loading_bay = reflect_2d_y(self.loading_bay)
         self.opp_side_cargo_bay = reflect_2d_y(self.side_cargo_bay)
-        self.opp_cross_point = (4.7, -1.8, 2)
+        self.opp_cross_point = (4.7, -1.8, 0, 2)
 
         self.start_pos = (
             1.2 + SwerveChassis.LENGTH / 2,
             0 + SwerveChassis.WIDTH / 2,
-            0.2,
+            0,
+            2,
         )
         self.start_speedup = (
-            1.4 + SwerveChassis.LENGTH / 2,
+            2.5 + SwerveChassis.LENGTH / 2,
             0 + SwerveChassis.WIDTH / 2,
+            0,
             2,
         )
         self.completed_runs = 0
         self.desired_angle = 0
         self.desired_angle_navx = 0
         self.minimum_path_completion = 0.85
-        self.loops = 0
 
         self.pursuit = PurePursuit(look_ahead=0.2)
 
@@ -75,16 +74,14 @@ class AutoBase(AutonomousStateMachine):
     def drive_to_cargo_bay(self, initial_call):
         if initial_call:
             # print(f"odometry = {self.current_pos}")
-            self.loops = 0
             if self.completed_runs == 0:
                 self.pursuit.build_path(
-                    (self.start_pos, self.start_speedup, self.front_cargo_bay)
+                    (self.current_pos, self.start_speedup, self.front_cargo_bay)
                 )
-                self.chassis.set_heading_sp(0)
             elif self.completed_runs == 1:
-                self.chassis.set_heading_sp(0)
                 self.pursuit.build_path(
                     (
+                        self.current_pos,
                         self.loading_bay,
                         self.loading_bay_speedup_point,
                         self.side_cargo_bay_slowdown_point,
@@ -93,10 +90,7 @@ class AutoBase(AutonomousStateMachine):
                 )
             elif self.completed_runs == 2:
                 self.chassis.set_heading_sp(math.pi)
-                self.pursuit.build_path((self.opp_loading_bay, self.opp_side_cargo_bay))
-        elif self.loops > 10 and self.completed_runs == 0:
-            self.chassis.set_heading_sp(math.pi / 2)
-        self.loops += 1
+                self.pursuit.build_path((self.current_pos, self.opp_loading_bay, self.opp_side_cargo_bay))
         if self.pursuit.completed_path and self.completed_runs > 3:
             self.next_state("stop")
         self.follow_path()
@@ -108,10 +102,7 @@ class AutoBase(AutonomousStateMachine):
 
     @state
     def deposit_hatch(self, initial_call):
-        # self.next_state("drive_to_loading_bay")
-        if initial_call:
-            self.hatchman.engage()
-            self.chassis.set_inputs(0, 0, 0)
+        self.hatch_deposit.engage()
         if not self.hatch.has_hatch:
             self.next_state("drive_to_loading_bay")
 
@@ -119,18 +110,16 @@ class AutoBase(AutonomousStateMachine):
     def drive_to_loading_bay(self, initial_call):
         if initial_call:
             if self.completed_runs == 1:
-                self.chassis.set_heading_sp(-math.pi / 2)
                 self.pursuit.build_path(
-                    (self.front_cargo_bay, self.setup_loading_bay, self.loading_bay)
+                    (self.current_pos, (self.current_pos[0]-0.5, self.current_pos[1], self.imu.getAngle(), 1.5), self.setup_loading_bay, self.loading_bay)
                 )
             elif self.completed_runs == 2:
                 # we only have a quater field, stop here
                 self.next_state("stop")
                 return
-                self.chassis.set_heading_sp(-math.pi / 2)
                 self.pursuit.build_path(
                     (
-                        self.side_cargo_bay,
+                        self.current_pos,
                         self.cross_point,
                         self.opp_cross_point,
                         self.opp_loading_bay,
@@ -138,8 +127,9 @@ class AutoBase(AutonomousStateMachine):
                 )
             elif self.completed_runs == 3:
                 # return to the loading bay for start of teleop
-                self.chassis.set_heading_sp(-math.pi / 2)
-                self.pursuit.build_path((self.opp_side_cargo_bay, self.opp_loading_bay))
+                self.pursuit.build_path((self.current_pos, self.opp_loading_bay))
+        if self.pursuit.distance_traveled > 0.5:
+            self.hatch.clear_to_retract = True
         if self.pursuit.completed_path and self.completed_runs > 3:
             self.next_state("stop")
         self.follow_path()
@@ -150,11 +140,8 @@ class AutoBase(AutonomousStateMachine):
 
     @state
     def intake_hatch(self, initial_call):
-        # self.next_state("drive_to_cargo_bay")
-        self.align.engage()
-        if initial_call:
-            self.chassis.set_inputs(0, 0, 0)
-        if self.hatch.has_hatch:
+        self.intake_hatch.engage()
+        if not self.intake_hatch.is_executing:
             self.next_state("drive_to_loading_bay")
 
     @state
@@ -164,7 +151,7 @@ class AutoBase(AutonomousStateMachine):
 
     @property
     def current_pos(self):
-        return (self.chassis.odometry_x, self.chassis.odometry_y, self.imu.getAngle())
+        return (self.chassis.odometry_x, self.chassis.odometry_y, self.imu.getAngle(), 2)
 
     def follow_path(self):
         vx, vy, heading = self.pursuit.find_velocity(self.current_pos)
@@ -172,13 +159,11 @@ class AutoBase(AutonomousStateMachine):
             self.chassis.set_inputs(0, 0, 0, field_oriented=False)
             return
         # TODO implement a system to allow for rotation in waypoints
+        self.chassis.set_heading_sp(heading)
         self.chassis.set_inputs(vx, vy, 0)
 
     def ready_for_vision(self):
-        if (
-            self.pursuit.distance_traveled / self.pursuit.waypoints[-1][3]
-            > self.minimum_path_completion
-        ):
+        if self.pursuit.waypoints[-1][4] - self.pursuit.distance_traveled < 1:
             return True
         else:
             return False
@@ -204,3 +189,4 @@ class RightStartAuto(AutoBase):
 
 class LeftStartAuto(AutoBase):
     MODE_NAME = "Left start autonomous"
+    DEFAULT = True
