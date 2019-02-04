ed30d1ef9d48e304f5e7895543685e2ebf4aeb03
michael <michael.kennedy@thedropbears.org.au>
2019-02-04 18:30:31 +1100

    Add delay to firing lower pistons

--- components/hatch.py
+++ components/hatch.py
@@ -9,9 +9,9 @@ class Hatch:
 
     chassis: SwerveChassis
 
-    bottom_puncher: wpilib.Solenoid
-    left_puncher: wpilib.Solenoid
-    right_puncher: wpilib.Solenoid
+    hatch_bottom_puncher: wpilib.Solenoid
+    hatch_left_puncher: wpilib.Solenoid
+    hatch_right_puncher: wpilib.Solenoid
 
     top_limit_switch: wpilib.DigitalInput
     left_limit_switch: wpilib.DigitalInput
@@ -22,23 +22,33 @@ class Hatch:
         self.has_hatch = True
         self.clear_to_retract = False
         self.fired_position = 0, 0
+        self.loop_counter = 0
 
     def execute(self):
         """Run at the end of every control loop iteration."""
-        self.bottom_puncher.set(self._punch_on)
-        self.left_puncher.set(self._punch_on)
-        self.right_puncher.set(self._punch_on)
-        if self.is_contained():
+        delay = 0
+        self.hatch_bottom_puncher.set(self._punch_on)
+        self.hatch_left_puncher.set(self._punch_on and self.loop_counter > delay)
+        self.hatch_right_puncher.set(self._punch_on and self.loop_counter > delay)
+        if self._punch_on and self.loop_counter > delay:
+            self.has_hatch = False
+        self.loop_counter += 1
+        if self.is_contained() and self.clear_to_retract:
             self.has_hatch = True
         if self.clear_to_retract:
             self._retract()
-        if math.hypot(self.fired_position[0] - self.chassis.position[0],
-                      self.fired_position[1] - self.chassis.position[1]) > 0.5:
+        if (
+            math.hypot(
+                self.fired_position[0] - self.chassis.position[0],
+                self.fired_position[1] - self.chassis.position[1],
+            )
+            > 0.5
+        ):
             self.clear_to_retract = True
 
     def punch(self):
+        self.loop_counter = 0
         self._punch_on = True
-        self.has_hatch = False
         self.clear_to_retract = False
         self.fired_position = self.chassis.position
 
