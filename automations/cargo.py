f5d0ddcdc2be8058774854a9305b3e24e28eaf1b
James Ward <j.ward@acfr.usyd.edu.au>
2019-02-19 13:29:45 +1100

    Cleanup logic of cargo state machine

--- automations/cargo.py
+++ automations/cargo.py
@@ -13,8 +13,8 @@ class CargoManager(StateMachine):
     @state(first=True, must_finish=True)
     def move_to_floor(self, initial_call, state_tm):
         self.cargo_component.move_to(Height.FLOOR)
-        if self.cargo_component.at_height(Height.FLOOR):
-            self.next_state("intaking_cargo")
+        self.cargo_component.intake()
+        self.next_state("intaking_cargo")
 
     def outake_cargo_ship(self, force=False):
         self.engage(initial_state="move_to_cargo_ship", force=force)
@@ -31,13 +31,12 @@ class CargoManager(StateMachine):
     @state(must_finish=True)
     def move_to_loading_station(self, initial_call, state_tm):
         self.cargo_component.move_to(Height.LOADING_STATION)
-        if self.cargo_component.at_height(Height.LOADING_STATION):
-            self.next_state("intaking_cargo")
+        self.cargo_component.intake()
+        self.next_state("intaking_cargo")
 
     @state(must_finish=True)
     def intaking_cargo(self):
         if self.cargo_component.is_contained():
-            self.cargo_component.stop()
             self.done()
         else:
             self.cargo_component.intake()
@@ -48,5 +47,10 @@ class CargoManager(StateMachine):
             self.cargo_component.outtake()
 
         if state_tm > 0.5:
-            self.cargo_component.stop()
             self.done()
+
+    def done(self):
+        super().done()
+        self.cargo_component.stop()
+        self.cargo_component.move_to(Height.LOADING_STATION)
+
