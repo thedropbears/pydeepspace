fc7f555b03440a955a96d0bc76219fef145f606a
David Vo <auscompgeek@users.noreply.github.com>
2019-03-05 19:25:29 +1100

    vision: Add type hints

--- components/vision.py
+++ components/vision.py
@@ -1,14 +1,12 @@
 import time
 
 from collections import deque
+from typing import Deque, NamedTuple, Tuple
 
 from networktables import NetworkTables
 from networktables.util import ntproperty
 
 from pyswervedrive.chassis import SwerveChassis
-
-from typing import NamedTuple
-
 from utilities.functions import rotate_vector
 
 
@@ -23,24 +21,25 @@ class Vision:
 
     chassis: SwerveChassis
 
-    fiducial_x = ntproperty("/vision/fiducial_x", 0.0, writeDefault=False)
-    fiducial_y = ntproperty("/vision/fiducial_y", 0.0, writeDefault=False)
-    fiducial_time = ntproperty("/vision/fiducial_time", -1.0, writeDefault=False)
-    ping_time = ntproperty("/vision/ping", 0.0, writeDefault=False)
-    raspi_pong_time = ntproperty("/vision/raspi_pong", 0.0, writeDefault=False)
-    rio_pong_time = ntproperty("/vision/rio_pong", 0.0, writeDefault=False)
-    latency = ntproperty("/vision/clock_offset", 0.0)
-    processing_time = ntproperty("/vision/processing_time", 0.0)
-    camera = ntproperty("/vision/game_piece", 0)  # 0 - hatch, 1 - cargo
+    fiducial_x: float = ntproperty("/vision/fiducial_x", 0.0, writeDefault=False)
+    fiducial_y: float = ntproperty("/vision/fiducial_y", 0.0, writeDefault=False)
+    fiducial_time: float = ntproperty("/vision/fiducial_time", -1.0, writeDefault=False)
+    ping_time: float = ntproperty("/vision/ping", 0.0, writeDefault=False)
+    raspi_pong_time: float = ntproperty("/vision/raspi_pong", 0.0, writeDefault=False)
+    rio_pong_time: float = ntproperty("/vision/rio_pong", 0.0, writeDefault=False)
+    latency: float = ntproperty("/vision/clock_offset", 0.0)
+    processing_time: float = ntproperty("/vision/processing_time", 0.0)
+    camera: float = ntproperty("/vision/game_piece", 0)  # 0 - hatch, 1 - cargo
     # NOTE: x and y are relative to the robot co-ordinate system, not the camera
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.latency = 0.0
         self.last_pong = time.monotonic()
-        self.odometry = deque(maxlen=50 * 2)  # 50Hz control loop for 2 secs
+        # 50Hz control loop for 2 seconds
+        self.odometry: Deque[Odometry] = deque(maxlen=50 * 2)
         NetworkTables.setUpdateRate(1)  # ensure our flush calls flush immediately
 
-    def execute(self):
+    def execute(self) -> None:
         """Store the current odometry in the queue. Allows projection of target into current position."""
         self.odometry.appendleft(
             Odometry(
@@ -57,10 +56,10 @@ class Vision:
         NetworkTables.flush()
 
     @property
-    def fiducial_in_sight(self):
+    def fiducial_in_sight(self) -> bool:
         return time.monotonic() - (self.fiducial_time + self.latency) < 0.1
 
-    def get_fiducial_position(self):
+    def get_fiducial_position(self) -> Tuple[float, float, float]:
         """Return the position of the retroreflective fiducials relative to the current robot pose."""
         vision_time = self.fiducial_time + self.latency
         vision_delta_x, vision_delta_y, vision_delta_heading = self._get_pose_delta(
@@ -70,10 +69,9 @@ class Vision:
         y = self.fiducial_y - vision_delta_y
         return x, y, vision_delta_heading
 
-    def _get_pose_delta(self, t):
+    def _get_pose_delta(self, t: float) -> Tuple[float, float, float]:
         """Search the stored odometry and return the position difference between now and the specified time."""
         current = self.odometry[0]
-        previous = self.odometry[0]
         for odom in self.odometry:
             if odom.t >= t:
                 previous = odom
@@ -88,11 +86,11 @@ class Vision:
         robot_x, robot_y = rotate_vector(x, y, -heading)
         return robot_x, robot_y, current.heading - heading
 
-    def ping(self):
+    def ping(self) -> None:
         """Send a ping to the RasPi to determine the connection latency."""
         self.ping_time = time.monotonic()
 
-    def pong(self):
+    def pong(self) -> None:
         """Receive a pong from the RasPi to determine the connection latency."""
         if abs(self.rio_pong_time - self.last_pong) > 1e-4:  # Floating point comparison
             alpha = 0.0  # Exponential averaging
