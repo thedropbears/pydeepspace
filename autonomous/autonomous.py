d69438fd981284b93c4f0c4da596553843214163
michael <michael.kennedy@thedropbears.org.au>
2019-03-11 14:51:44 +1100

    Autonomous: refactor to use dataclass coordinates

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 import math
 
 from magicbot.state_machine import AutonomousStateMachine, state
+import wpilib
 
 from automations.alignment import HatchDepositAligner, HatchIntakeAligner
 from components.hatch import Hatch
@@ -8,6 +10,38 @@ from components.vision import Vision
 from pyswervedrive.chassis import SwerveChassis
 from utilities.navx import NavX
 from utilities.pure_pursuit import PurePursuit, Waypoint, insert_trapezoidal_waypoints
+
+
+@dataclass
+class Coordinates:
+    start_pos: Waypoint
+    front_cargo_bay: Waypoint
+    setup_loading_bay: Waypoint
+    loading_bay: Waypoint
+    side_cargo_bay_alignment_point: Waypoint
+    side_cargo_bay: Waypoint
+
+
+left_coordinates = Coordinates(
+    start_pos=Waypoint(
+        1.2 + SwerveChassis.LENGTH / 2, 0 + SwerveChassis.WIDTH / 2, 0, 2
+    ),
+    front_cargo_bay=Waypoint(5.5 - SwerveChassis.LENGTH / 2, 0.3, 0, 0.75),
+    setup_loading_bay=Waypoint(3.3, 3.3, math.pi, 1),
+    loading_bay=Waypoint(0.2 + SwerveChassis.LENGTH / 2, 3.4, math.pi, 1),
+    side_cargo_bay_alignment_point=Waypoint(
+        6.6, 1.8 + SwerveChassis.WIDTH / 2, -math.pi / 2, 0.75
+    ),
+    side_cargo_bay=Waypoint(6.6, 0.8 + SwerveChassis.WIDTH / 2, -math.pi / 2, 1),
+)
+right_coordinates = Coordinates(
+    start_pos=left_coordinates.start_pos.reflect(),
+    front_cargo_bay=left_coordinates.front_cargo_bay.reflect(),
+    setup_loading_bay=left_coordinates.setup_loading_bay.reflect(),
+    loading_bay=left_coordinates.loading_bay.reflect(),
+    side_cargo_bay_alignment_point=left_coordinates.side_cargo_bay_alignment_point.reflect(),
+    side_cargo_bay=left_coordinates.side_cargo_bay.reflect()
+)
 
 
 class AutoBase(AutonomousStateMachine):
@@ -26,18 +60,8 @@ class AutoBase(AutonomousStateMachine):
 
     def __init__(self):
         super().__init__()
-        self.front_cargo_bay = Waypoint(5.6 - SwerveChassis.LENGTH / 2, 0.2, 0, 0.75)
-        self.setup_loading_bay = Waypoint(3.3, 3.3, math.pi, 2)
-        self.loading_bay = Waypoint(0.2 + SwerveChassis.LENGTH / 2, 3.4, math.pi, 1)
-        self.side_cargo_bay = Waypoint(
-            7, 0.8 + SwerveChassis.WIDTH / 2, -math.pi / 2, 1
-        )
-        self.side_cargo_bay_alignment_point = Waypoint(
-            7, 1.8 + SwerveChassis.WIDTH / 2, -math.pi / 2, 0.75
-        )
-        self.start_pos = Waypoint(
-            1.2 + SwerveChassis.LENGTH / 2, 0 + SwerveChassis.WIDTH / 2, 0, 2
-        )
+        self.coordinates: Coordinates
+        self.coordinates = left_coordinates
 
         self.completed_runs = 0
         self.desired_angle = 0
@@ -51,21 +75,21 @@ class AutoBase(AutonomousStateMachine):
 
     def setup(self):
         self.hatch.has_hatch = True
+        self.hatch_intake.alignment_speed = 1.2
+        self.hatch_deposit.alignment_speed = 1.2
 
     def on_enable(self):
         super().on_enable()
-        self.chassis.odometry_x = self.start_pos.x
-        self.chassis.odometry_y = self.start_pos.y
+        self.chassis.odometry_x = self.coordinates.start_pos.x
+        self.chassis.odometry_y = self.coordinates.start_pos.y
         self.completed_runs = 0
-        # print(f"odometry = {self.current_pos}")
 
     @state(first=True)
     def drive_to_cargo_bay(self, initial_call):
         if initial_call:
-            # print(f"odometry = {self.current_pos}")
             if self.completed_runs == 0:
                 waypoints = insert_trapezoidal_waypoints(
-                    (self.current_pos, self.front_cargo_bay),
+                    (self.current_pos, self.coordinates.front_cargo_bay),
                     self.acceleration,
                     self.deceleration,
                 )
@@ -73,8 +97,8 @@ class AutoBase(AutonomousStateMachine):
                 waypoints = insert_trapezoidal_waypoints(
                     (
                         self.current_pos,
-                        self.side_cargo_bay_alignment_point,
-                        self.side_cargo_bay,
+                        self.coordinates.side_cargo_bay_alignment_point,
+                        self.coordinates.side_cargo_bay,
                     ),
                     self.acceleration,
                     self.deceleration,
@@ -111,15 +135,15 @@ class AutoBase(AutonomousStateMachine):
                             self.imu.getAngle(),
                             1.5,
                         ),
-                        self.setup_loading_bay,
-                        self.loading_bay,
+                        self.coordinates.setup_loading_bay,
+                        self.coordinates.loading_bay,
                     ),
                     self.acceleration,
                     self.deceleration,
                 )
             elif self.completed_runs == 2:
                 waypoints = insert_trapezoidal_waypoints(
-                    (self.current_pos, self.setup_loading_bay, self.loading_bay),
+                    (self.current_pos, self.coordinates.setup_loading_bay, self.coordinates.loading_bay),
                     self.acceleration,
                     self.deceleration,
                 )
@@ -156,7 +180,6 @@ class AutoBase(AutonomousStateMachine):
         if self.pursuit.completed_path:
             self.chassis.set_inputs(0, 0, 0, field_oriented=True)
             return
-        # TODO implement a system to allow for rotation in waypoints
         self.chassis.set_velocity_heading(vx, vy, heading)
 
     def ready_for_vision(self):
@@ -166,21 +189,16 @@ class AutoBase(AutonomousStateMachine):
             return False
 
 
-class RightStartAuto(AutoBase):
-    MODE_NAME = "Right start autonomous"
+class RightFullAuto(AutoBase):
+    MODE_NAME = "Right Full Autonomous"
 
     def __init__(self):
         super().__init__()
-        self.front_cargo_bay = self.front_cargo_bay.reflect()
-        self.setup_loading_bay = self.setup_loading_bay.reflect()
-        self.loading_bay = self.loading_bay.reflect()
-        self.side_cargo_bay = self.side_cargo_bay.reflect()
-        self.side_cargo_bay_alignment_point = (
-            self.side_cargo_bay_alignment_point.reflect()
-        )
-        self.start_pos = self.start_pos.reflect()
+        self.coordinates = right_coordinates
 
 
-class LeftStartAuto(AutoBase):
-    MODE_NAME = "Left start autonomous"
-    DEFAULT = True
+class LeftFullAuto(AutoBase):
+    MODE_NAME = "Left Full Autonomous"
+
+
+
